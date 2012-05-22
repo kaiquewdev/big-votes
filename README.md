0833cd292733f36ab353a042c0d97f5cc523b284
Kaique da Silva <kaique.developer@gmail.com>
2012-05-22 00:49:34 -0300

    Run add to bootstrap

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-big-votes
-=========
+Big Votes Challenge
+===================
+
+Requirements:
+    python 2.7
+How to use:
+    " Execute the file named run.py using python cli and acess http://localhost:8000
+    python run.py
---
+++ run.py
@@ -0,0 +1,27 @@
+#!/bin/python
+# -*- codind:utf-8
+import os
+
+config = {
+    'command': './web2py/web2py.py',
+    'argument': '-a',
+    'password': 'dev',
+    'domain': 'htpp://localhost',
+    'port': '8000',
+}
+
+def main():
+    try:
+        print '-'*100
+        print 'Acesse: %(domain)s:%(port)s' % ( config )
+        print '-'*100
+        print '\n'
+        
+        run = os.system( '%(command)s %(argument)s%(password)s' % ( config ) )
+        return True
+    except Exception:
+        print 'Erro ao executar commando tente manualmente usand: %(command)s %(argument)s%(password)s' % (config)
+        return False
+
+if __name__ == '__main__':
+    main()
